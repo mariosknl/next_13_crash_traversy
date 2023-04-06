04dfb30b7663a6d80dc843616728732f60223af3
Marios Kanellopoulos <mariosknl@outlook.com>
2023-04-06 09:17:03 +0300

    search results

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,9 @@
-import Image from "next/image";
+"use client";
+import { useState, useEffect } from "react";
 import { Inter } from "next/font/google";
+import LoadingPage from "./loading";
 import Courses from "./components/Courses";
+import CourseSearch from "./components/CourseSearch";
 
 const inter = Inter({
 	weight: ["400", "600", "700"],
@@ -8,10 +11,29 @@ const inter = Inter({
 });
 
 const HomePage = () => {
+	const [courses, setCourses] = useState([]);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const fetchCourses = async () => {
+			const res = await fetch("/api/courses");
+			const data = await res.json();
+			setCourses(data);
+			setLoading(false);
+		};
+
+		fetchCourses();
+	}, []);
+
+	if (loading) {
+		return <LoadingPage />;
+	}
+
 	return (
 		<>
 			<h1>Welcome to Traversy Media</h1>
-			<Courses />
+			<CourseSearch getSearchResults={(results) => setCourses(results)} />
+			<Courses courses={courses} />
 		</>
 	);
 };
