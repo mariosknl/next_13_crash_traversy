6a7be05709b48dc2c95d48473d0de3545a8170f7
Marios Kanellopoulos <mariosknl@outlook.com>
2023-04-06 08:54:41 +0300

    fetch endpoint created

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import Link from "next/link";
+import Courses from "./components/Courses";
 
 const inter = Inter({
 	weight: ["400", "600", "700"],
@@ -9,20 +9,10 @@ const inter = Inter({
 
 const HomePage = () => {
 	return (
-		<div>
+		<>
 			<h1>Welcome to Traversy Media</h1>
-			<ul>
-				<li>
-					<Link href="/">Home</Link>
-				</li>
-				<li>
-					<Link href="/about">About</Link>
-				</li>
-				<li>
-					<Link href="/about/team">Team</Link>
-				</li>
-			</ul>
-		</div>
+			<Courses />
+		</>
 	);
 };
 
