798f5835ec0f61082e5af2616dd7f97216b763b9
Marios Kanellopoulos <mariosknl@outlook.com>
2023-04-06 08:41:30 +0300

    suspense boundaries

---
+++ app/components/Repo.jsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import { FaStar, FaCodeBranch, FaEye } from "react-icons/fa";
+
+async function fetchRepo(name) {
+	const response = await fetch(
+		`https://api.github.com/repos/mariosknl/${name}`
+	);
+	const repo = await response.json();
+	return repo;
+}
+
+const Repo = async ({ name }) => {
+	const repo = await fetchRepo(name);
+
+	return (
+		<>
+			<h2>{repo.name}</h2>
+			<p>{repo.description}</p>
+			<div className="card-stats">
+				<div className="cart-stat">
+					<FaStar />
+					<span>{repo.stargazers_count}</span>
+				</div>
+				<div className="cart-stat">
+					<FaCodeBranch />
+					<span>{repo.forks_count}</span>
+				</div>
+				<div className="cart-stat">
+					<FaEye />
+					<span>{repo.watchers_count}</span>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Repo;
